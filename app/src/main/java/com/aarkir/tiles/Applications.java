2f82abe93029089342edf7bd9a98346258404087
Aaron Kirtland <aaronk3.14@gmail.com>
2016-04-23 20:04:24 -0500

    Launcher now only shows the correct apps
    Made the apps that appear reliant on the apps having a resolve info that
    takes up the home screen

--- app/src/main/java/com/aarkir/tiles/Applications.java
+++ app/src/main/java/com/aarkir/tiles/Applications.java
@@ -4,13 +4,10 @@ Holds data about the apps currently installed on the device
 
 package com.aarkir.tiles;
 
-import android.app.Application;
 import android.content.Intent;
-import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
-import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -25,109 +22,53 @@ public class Applications {
     public Applications(PackageManager packManager){
         mainIntent = new Intent(Intent.ACTION_MAIN, null).addCategory(Intent.CATEGORY_LAUNCHER);
         packMan = packManager;
+        activityList = this.createActivityList(); //used to find intent data to launch application
         packageList = this.createPackageList();
-        activityList = this.createActivityList();
-        this.addClassNamesToPackageList();
     }
 
     public ArrayList getPackageList(){
         return packageList;
     }
 
-    /*
-    public List getActivityList(){
-        return activityList;
-    }
-    */
-
-    private ArrayList<AppInfo> createPackageList(){
+    private ArrayList<AppInfo> createPackageList() {
         //list to be returned with the apps added
         ArrayList<AppInfo> pList = new ArrayList<>();
 
-        //for each package
+        //list of packages
         List<PackageInfo> packs = packMan.getInstalledPackages(0);
 
+        //for each package
         for(PackageInfo pack : packs) {
-            //Try to get the application info for the app
-            /*
-            try {
-                ApplicationInfo ai = packMan.getApplicationInfo(pack.packageName, 0);
-                if ((ai.flags & ApplicationInfo.FLAG_SYSTEM) != 0 && (ai.flags & ApplicationInfo.FLAG_UPDATED_SYSTEM_APP) == 0) {
-                    continue;
+            //for each activity
+            for (ResolveInfo activity : activityList) {
+                //if the package name is in the activity list
+                if (activity.activityInfo.applicationInfo.packageName.equals(pack.packageName)) {
+                    //container for the app data
+                    AppInfo newInfo = new AppInfo();
+
+                    //set the app data info based on the package info
+                    newInfo.setAppName(pack.applicationInfo.loadLabel(packMan).toString());
+                    newInfo.setPackageName(pack.packageName);
+                    newInfo.setVersionName(pack.versionName);
+                    newInfo.setVersionCode(pack.versionCode);
+                    newInfo.setIcon(pack.applicationInfo.loadIcon(packMan));
+                    newInfo.setClassName(activity.activityInfo.name);
+
+                    pList.add(newInfo);
+
+                    break;
                 }
             }
-            catch (Exception e) {
-                Log.e("BACKGROUND PROC:", e.getMessage());
-            }
-            */
-
-            //container for the app data
-            AppInfo newInfo = new AppInfo();
-
-            //set the app data info based on the package info
-            newInfo.setAppName(pack.applicationInfo.loadLabel(packMan).toString());
-            newInfo.setPackageName(pack.packageName);
-            newInfo.setVersionName(pack.versionName);
-            newInfo.setVersionCode(pack.versionCode);
-            newInfo.setIcon(pack.applicationInfo.loadIcon(packMan));
-
-            pList.add(newInfo);
         }
         return pList;
     }
 
     private List<ResolveInfo> createActivityList() {
+        //get the list of resolve info
         List<ResolveInfo> aList = packMan.queryIntentActivities(mainIntent, PackageManager.GET_META_DATA);
-
+        //sort the resolve info list
         Collections.sort(aList, new ResolveInfo.DisplayNameComparator(packMan));
 
         return aList;
-    }
-
-    /*
-    private void packageDebug(){
-        if(null == packageList){
-            return;
-        }
-
-        for(AppInfo app : packageList){
-            Log.v("PACKINFO: ", "\t" +
-                    app.getAppName() + "\t" +
-                    app.getPackageName() + "\t" +
-                    app.getClassName() + "\t" +
-                    app.getVersionName() + "\t" +
-                    app.getVersionCode());
-        }
-    }
-    */
-
-    /*
-    private void activityDebug(){
-        if(null == activityList){
-            return;
-        }
-
-        for(ResolveInfo activity : activityList){
-            ActivityInfo currentActivity = activity.activityInfo;
-            Log.v("ACTINFO",
-                    "pName="
-                            + currentActivity.applicationInfo.packageName +
-                            " cName=" + currentActivity.name);
-        }
-    }
-    */
-
-    private void addClassNamesToPackageList() {
-        if(null == activityList || null == packageList){
-            return;
-        }
-
-        for(AppInfo app : packageList) {
-            for(ResolveInfo activity : activityList) {
-                if(app.getPackageName().equals(activity.activityInfo.applicationInfo.packageName)) {
-                    app.setClassName(activity.activityInfo.name);
-                }
-            }
-        }
     }
 }
